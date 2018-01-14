94aa7f69f47cd4fa145e0490aa000c65198e8f82
Chris Wright <daverandom@php.net>
2018-01-14 15:42:23 +0000

    Remove pointless map definition

--- src/functions.php
+++ src/functions.php
@@ -15,37 +15,6 @@ const UNBOUNDED = -1;
 \define(__NAMESPACE__ . '\\FLOAT_WIDTH', FLOAT_SIZE * 8);
 \define(__NAMESPACE__ . '\\DOUBLE_WIDTH', DOUBLE_SIZE * 8);
 
-\define(__NAMESPACE__ . '\\ELEMENT_SIZES', [
-    TypeCodes::INT_SYS    => INT_SIZE,
-    TypeCodes::UINT_SYS   => INT_SIZE,
-
-    TypeCodes::INT8       => 1,
-    TypeCodes::UINT8      => 1,
-
-    TypeCodes::INT16_SYS  => 2,
-    TypeCodes::UINT16     => 2,
-    TypeCodes::UINT16_LE  => 2,
-    TypeCodes::UINT16_SYS => 2,
-
-    TypeCodes::INT32_SYS  => 4,
-    TypeCodes::UINT32     => 4,
-    TypeCodes::UINT32_LE  => 4,
-    TypeCodes::UINT32_SYS => 4,
-
-    TypeCodes::INT64_SYS  => 8,
-    TypeCodes::UINT64     => 8,
-    TypeCodes::UINT64_LE  => 8,
-    TypeCodes::UINT64_SYS => 8,
-
-    TypeCodes::FLOAT      => FLOAT_SIZE,
-    TypeCodes::FLOAT_LE   => FLOAT_SIZE,
-    TypeCodes::FLOAT_SYS  => FLOAT_SIZE,
-
-    TypeCodes::DOUBLE     => DOUBLE_SIZE,
-    TypeCodes::DOUBLE_LE  => DOUBLE_SIZE,
-    TypeCodes::DOUBLE_SYS => DOUBLE_SIZE,
-]);
-
 const INT_CODES_BY_WIDTH = [
     8  => TypeCodes::INT8,
     16 => TypeCodes::INT16_SYS,
